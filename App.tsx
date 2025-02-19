4ffa4ed7c93750cceea5aa3340024b035a6c020d
mega025 <xinjiepan123@gmail.com>
2025-02-19 10:17:31 +0100

    diseño home bien, falta funcional de botones el careusel

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import {NavigationContainer} from "@react-navigation/native";
-import {LoginScreen} from "./app/presentation/views/Login";
+import {LoginScreen} from "./app/presentation/views/auth/Login";
 import {createStackNavigator} from "@react-navigation/stack";
+import {Home} from "./app/presentation/views/home/Home";
 
 const Stack = createStackNavigator<RootStackParamsList>();
 
 export type RootStackParamsList = {
   LoginScreen: undefined;
+  Home:undefined;
 
 }
 
@@ -15,7 +17,8 @@ export default function App() {
   return (
       <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="LoginScreen" component={LoginScreen}/>
+                {/*<Stack.Screen name="LoginScreen" component={LoginScreen}/>*/}
+                <Stack.Screen name="Home" component={Home}/>
           </Stack.Navigator>
       </NavigationContainer>
   );
