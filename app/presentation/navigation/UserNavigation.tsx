b82b436eff312433aec70d3a7f1dad780ed784ce
mega025 <xinjiepan123@gmail.com>
2025-02-19 10:31:28 +0100

    pasando logitos

---
+++ app/presentation/navigation/UserNavigation.tsx
@@ -0,0 +1,19 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Home} from "../views/home/Home";
+import {Image} from "react-native";
+
+const Tab = createBottomTabNavigator();
+
+export const userNavigation=() =>{
+return (
+//     <Tab.Navigator>
+//         <Tab.Screen name="Home" options={{title:"Home",
+//         tabBarLabel:"Home",
+//         tabBarIcon: ({color}) =>(
+// <Image source={require("")}
+//         )}}></Tab.Screen>
+
+
+    </Tab.Navigator>
+)
+}
