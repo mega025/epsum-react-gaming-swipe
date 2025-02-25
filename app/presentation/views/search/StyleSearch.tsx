ba34e17d06577a105e443cc1ae146702e7b9d52b
mega025 <xinjiepan123@gmail.com>
2025-02-25 13:49:10 +0100

    barra de busqueda casi terminado queda definir bien el genero y la fecha

--- app/presentation/views/search/StyleSearch.tsx
+++ app/presentation/views/search/StyleSearch.tsx
@@ -3,7 +3,6 @@ import { AppColors } from "../../theme/AppTheme";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { RFPercentage } from "react-native-responsive-fontsize";
 
-const { width, height } = Dimensions.get("window");
 
 const styleSearch = StyleSheet.create({
     logo: {
@@ -29,7 +28,6 @@ const styleSearch = StyleSheet.create({
     title: {
         fontSize: RFPercentage(4),
         alignSelf: "center",
-        marginTop: hp("2%"),
         color: AppColors.white,
         fontFamily: "zen_kaku_light",
     },
@@ -41,7 +39,7 @@ const styleSearch = StyleSheet.create({
         paddingHorizontal: wp("3%"),
         paddingVertical: hp("1%"),
         alignSelf: "center",
-        marginTop: hp("5.5%"),
+        marginTop: hp("4%"),
     },
     icon: {
         width: wp("6%"),
