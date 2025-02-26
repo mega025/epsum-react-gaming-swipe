b5c8546434700ab8ba4e421df7d6dce65ba28d7f
mega025 <xinjiepan123@gmail.com>
2025-02-26 14:06:22 +0100

    mejorado un poco el diseño de bsuqueda, falta averiguar porque hay imagenes que no hay

--- app/presentation/views/search/Search.tsx
+++ app/presentation/views/search/Search.tsx
@@ -5,61 +5,17 @@ import {
     Text,
     View,
     TouchableWithoutFeedback,
-    Keyboard, FlatList, ActivityIndicator,
-
-} from "react-native";
+    Keyboard, FlatList, ActivityIndicator,}
+from "react-native";
 import { CustomTextInputSearch } from "../../components/CustomTextInputSearch";
 import styleSearch from "./StyleSearch";
 import GameItems from "../../components/GameItems";
-import {IgdbApiDelivery} from "../../../data/sources/remote/igdbAPI/IgdbApiDelivery";
 import {Game} from "../../../domain/entities/Game";
-
+import viewModel from "./viewModel";
 
 export function Search() {
-    const [searchText, setSearchText] = useState("");
-    const [games, setGames] =useState<Game[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState(0);
 
-
-    const searchGames = async (text: string, page: number = 1) => {
-
-        setLoading(true);
-
-
-        try {
-            const res = await IgdbApiDelivery.post<Game[]>("/games",
-                `fields name, rating, platforms.abbreviation, genres.name, cover.url, release_dates.y; limit 15; search "${text}"; offset ${page  * 15};`
-            );
-            if (page === 1) {
-                setGames(res.data);
-            } else {
-                setGames((prevGames) => [...prevGames, ...res.data]);
-            }
-        } catch (error) {
-            console.error("Error al buscar juegos:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const SearchTextChange = (text: string) => {
-        setSearchText(text);
-        setPage(1);
-        searchGames(text, 1);
-        setLoading(true);
-
-    };
-
-    const LoadMoreGame = () => {
-        if (!loading) {
-            setPage((prevPage) => {
-                const nextPage = prevPage + 1;
-                searchGames(searchText, nextPage);
-                return nextPage;
-            });
-        }
-    };
+    const {games, setGames, loading, LoadMoreGame, SearchTextChange,searchText} = viewModel.searchViewModel()
 
     return (
         <View style={styleSearch.container}>
